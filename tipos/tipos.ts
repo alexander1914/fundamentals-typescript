a3e8fcb96eaeedb15af82dde85696491bb48fa4f
alexander1914 <alexander.oliveira99@gmail.com>
2024-07-16 23:58:53 +0100

    Tipos parte 2

--- tipos/tipos.ts
+++ tipos/tipos.ts
@@ -72,3 +72,98 @@ function meuDizimo(salario: number): number{
 
 console.log(meuDizimo(820));
 
+//Objeto do tipo any
+
+let usuario ={
+    nome: 'Jesus Cristo',
+    idade: 33
+}
+
+console.log(usuario);
+
+//Objeto com seus determinados tipos
+
+let usuario2: {nome: string, idade: number} = {
+    nome: 'Alexander Oliveira',
+    idade: 34
+}
+
+console.log(usuario2);
+
+let funcionario: {
+    // parte 1 fazendo a definição do objeto
+    supervisores: string [],
+    baterPonto:(horas: number) => string
+} = {
+    // parte 2 fazendo atribuição do objeto
+    supervisores: ['João', 'Fulano'],
+    baterPonto(horario: number): string {
+        if(horario <= 8){
+            return 'Ponto normal'
+        }else{
+            return 'Fora de horário'
+        }
+    },
+}
+
+console.log(funcionario.supervisores);
+console.log(funcionario.baterPonto(8));
+console.log(funcionario.baterPonto(9));
+
+//Type é possível criar um tipo para seu objeto
+type Funcionario = {
+    supervisores: string [],
+    baterPonto:(horas: number) => string
+}
+// Atribuindo pelo objeto Funcionario
+let funcionario2 : Funcionario = {
+    supervisores: ['Nathalia', 'Carla'],
+    baterPonto(horario: number): string {
+        if(horario <= 8){
+            return 'Ponto normal'
+        }else{
+            return 'Fora de horário'
+        }
+    },
+}
+
+//Union Types com este simbolo | é possível unir 2 tipos diferentes
+let nota: number | string = 10
+console.log(`Minha nota é ${nota} !`);
+nota = '8'
+console.log(`Minha nota é ${nota} !`);
+
+//Checando os tipos
+let valor = 30
+if(typeof valor === "number"){
+    console.log("É um number ...");    
+}else{
+    console.log(typeof valor);    
+}
+
+//Never é quando vai terminar com erro
+function falha(msg:string): never {
+    throw new Error(msg)
+} 
+
+const produto = {
+    nome: '',
+    preco: -0,
+    validarProduto(){
+        if(!this.nome || this.nome.trim().length == 0) {
+            falha('É necessário ter um nome')
+        }
+        if (this.preco < 0) {
+            falha('Preco inválido')
+        }
+    }
+}
+//console.log(produto.validarProduto());
+
+//Null como utilizar no typescript é necessário o uniontypes
+let numberOpcional: null | number = 11
+numberOpcional = null
+console.log(numberOpcional);
+
+
+
