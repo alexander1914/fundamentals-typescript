942c81e54ea15e7293f65de3c0a1aa5dad8e31dc
alexander1914 <alexander.oliveira99@gmail.com>
2024-07-17 23:07:19 +0100

    Aula de compilador - parte 1

---
+++ compilador/compilador.ts
@@ -0,0 +1,18 @@
+console.log('Aula de Compilador...');
+// é necessário alterar o tsconfig para definir algumas configurações
+//"noEmitOnError": true para não emitir js no caso de erro.
+
+let canal: string = 'Gaveta'
+let inscritos: number = 132145
+
+//canal = inscritos
+console.log(`Canal = ${canal}`);
+nome = 'Pedro'
+console.log(`Nome = ${nome}`);
+
+/* Language and Environment */
+//"target": "es2016",     
+// para definir a versão do javascript
+
+//"sourceMap" é criar um mapemento do codigo brower com o arquivo original.
+//"sourceMap": true,
