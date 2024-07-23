378e11f208b42b5aa31fd44b3b4f7c3eb9ce7e45
alexander1914 <alexander.oliveira99@gmail.com>
2024-07-23 16:52:08 +0100

    Arrow function, this, rest/spread e destructuring - parte 2

--- ecmascript/ecmascript.ts
+++ ecmascript/ecmascript.ts
@@ -31,5 +31,88 @@ for (var i = 0; i < 10; i++) {
     console.log(i);        
 }
 
+//Arrow Function: é uma função objetivo dela é ter uma sintax mais reduzida
+const somar = function(n1: number, n2: number): number{
+    return n1 + n2;
+}
+console.log(somar(2000,2500));
 
+const subtrair = (n1: number, n2:number) => n1 - n2
+console.log(subtrair(1000,900));
+
+const saudacao = () => console.log("Oi typescript...");
+saudacao()
+
+const falarCom = (pessoa: string) => console.log('Oi... ' + pessoa);
+falarCom('Nathália')
+
+//this
+function normalComThis(this: any) {
+    console.log(this);
+    
+}
+normalComThis()
+
+const normalComThisEspecial = normalComThis
+        .bind({nome: 'Alexander'})
+normalComThisEspecial()
+
+//const arrowComThis = () => console.log(this);
+//arrowComThis()
+
+//Parâmetros Padrão: ser caso você não definir o valor padrão
+function contagemRegressiva(inicio: number = 3,
+    fim: number = inicio - 5): void {
+    console.log(inicio);
+    console.log("Inicio");
+    
+    while (inicio >= fim) {
+        inicio--
+        console.log(inicio);        
+    }
+    console.log("Fim!");
+    
+}
+contagemRegressiva(3)
+
+//Rest & Spread
+
+//... Spread é para espalhar 
+const number = [1, 10, 99, -5]
+console.log(Math.max(...number));
+
+const turmaA: string[] = ['João', 'Maria', 'Fernanda']
+const turmaB: string[] = 
+      ['Jojo', 'Joice', 'Megan', ...turmaA]
+console.log(turmaB);
+
+//Rest é para juntar
+function retornarArray(...args: number[]): number[] {        
+    return args
+}
+
+const numeros = retornarArray(10, 20, 30, 50)
+console.log(numeros);
+
+const tupla: [number, string, boolean] = [1, 'abc', false]
+
+function tuplaParam1(a: number, b: string, c: boolean): void {
+    console.log(`1) ${a} ${b} ${c}`)    
+}
+
+tuplaParam1(...tupla)
+
+function tuplaParam2(...params: [number, string, boolean]) {
+    console.log(Array.isArray(params));
+    console.log(`2) ${params[0]} ${params[1]} ${params[2]}`);    
+}
+
+tuplaParam2(...tupla)
+
+//Destructuring (array)
+const caracteristica = ['Motor Zetec 1.8', 2020]
+
+const [motor, ano] = caracteristica
+console.log(motor);
+console.log(ano);
 
