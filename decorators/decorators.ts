211f88d1c5ad06d34e0acda929bd00350718e97a
alexander1914 <alexander.oliveira99@gmail.com>
2024-08-19 20:39:56 +0100

    Decorators - parte final

--- decorators/decorators.ts
+++ decorators/decorators.ts
@@ -61,3 +61,107 @@ function imprimivel(constructor: Function) {
 
 const eletro = new Eletrodomestico()
 eletro.imprimir && eletro.imprimir()
+console.log();
+
+console.log("Desafio Decorator...");
+const usuarioLogado = {
+    nome:  'Alexander Oliveira',
+    email: 'alexander.oliveira99@gmail.com',
+    admin: true
+}
+
+//Desafio
+//Decorator
+function perfilAdmin<T extends Construtor>(constructor: T) {
+    return class extends constructor {
+        constructor(...arg: any[]){
+            super(...arg)
+            if (!usuarioLogado || !usuarioLogado.admin) {
+                throw new Error('Sem permissão !')
+            }
+        }
+    }
+}
+
+@perfilAdmin
+class MudancaAdministrativa {
+    critico(){
+        console.log('Algo crítico foi alterado....');        
+    }
+}
+
+new MudancaAdministrativa().critico()
+console.log();
+
+console.log("Decorator para métodos");
+class ContaCorrent {
+    private saldo: number
+    
+    constructor(saldo: number) {
+        this.saldo = saldo
+    }
+
+    @congelar
+    sacar(@paramInfo valor: number){
+        if (valor <= this.saldo) {
+            this.saldo -= valor
+            return true
+        }else{
+            return false
+        }
+    }
+
+    @congelar
+    //@naoNegativo
+    getSaldo(){
+        return this.saldo
+    }
+}
+
+const cc = new ContaCorrent(1500)
+cc.sacar(150)
+console.log(cc.getSaldo());
+
+// Falha 
+/*
+cc.getSaldo = function() {
+    return this['saldo'] + 7000
+}
+console.log(cc.getSaldo());
+*/
+
+//Object.freeze()
+function congelar(alvo: any, nomePropriedade: string, 
+    descritor: PropertyDescriptor) {
+    console.log(alvo);
+    console.log(nomePropriedade);    
+    descritor.writable = false
+}
+console.log();
+console.log("Decorator de Atributo");
+
+function naoNegativo(alvo: any, nomePropriedade: string) {
+    delete alvo[nomePropriedade]
+    
+    Object.defineProperty(alvo, nomePropriedade, {
+        get: function(): any {
+            return alvo["_" + nomePropriedade]
+        },
+        set: function(valor: any): void {
+            if(valor < 0){
+                throw new Error('Saldo Inválido')
+            } else {
+                alvo["_" + nomePropriedade] = valor
+            }
+        }
+    })
+}
+console.log();
+
+console.log("Decorator por Param");
+function paramInfo(alvo: any, nomeMetodo: string,
+    indiceParam: number) {
+    console.log(`Alvo: ${alvo}`);
+    console.log(`Método: ${nomeMetodo}`);
+    console.log(`Índice Param: ${indiceParam}`);    
+}
